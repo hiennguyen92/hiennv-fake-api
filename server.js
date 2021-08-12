36921396bda14ecaccc7d1707e13cb9611c7c188
Hien Nguyen <hiennguyen92@gmail.com>
2021-08-13 06:10:44 +0700

    update permission

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const jsonServer = require('json-server')
+const auth = require('json-server-auth')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
 
-server.use(middlewares)
+const rules = auth.rewriter({
+    // Permission rules
+    users: 400,
+    profiles: 400,
+  })
+
+server.use(rules)
+server.use(auth)
+server.db = router.db
 server.use(router)
 server.listen(process.env.PORT || 3000, () => {
     console.log('Server is running...')
